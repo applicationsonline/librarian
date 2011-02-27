c92338fc6d1a2e92613590888a81c7302f43619a
Jay Feldblum <y_feldblum@yahoo.com>
2011-02-27 02:14:39 -0500

    Only need to define the method once.

--- lib/librarian/source/path.rb
+++ lib/librarian/source/path.rb
@@ -19,7 +19,15 @@ module Librarian
       def cache!(dependencies)
       end
 
-      def install!
+      def install!(dependency)
+        cache_path = dependency_cache_path(dependency)
+        install_path = dependency_install_path(dependency)
+        if install_path.exist?
+          debug { "Deleting #{relative_path_to(install_path)}" }
+          install_path.rmtree
+        end
+        debug { "Copying #{relative_path_to(cache_path)} to #{relative_path_to(install_path)}" }
+        FileUtils.cp_r(cache_path, install_path)
       end
 
       def manifest_search_paths(dependency)
@@ -37,17 +45,6 @@ module Librarian
 
       def dependency_install_path(dependency)
         root_module.install_path.join(dependency.name)
-      end
-
-      def install!(dependency)
-        cache_path = dependency_cache_path(dependency)
-        install_path = dependency_install_path(dependency)
-        if install_path.exist?
-          debug { "Deleting #{relative_path_to(install_path)}" }
-          install_path.rmtree
-        end
-        debug { "Copying #{relative_path_to(cache_path)} to #{relative_path_to(install_path)}" }
-        FileUtils.cp_r(cache_path, install_path)
       end
 
     private
