1284d14b6c9426bc3e4697035a73b219e9b4d828
Jay Feldblum <y_feldblum@yahoo.com>
2011-02-28 10:28:43 -0500

    The specfile knows the default source. This is the source where transitive dependencies may be found. If a transitive dependency is to be found in a non-default source, then it must be specified explicitly.

--- lib/librarian/specfile.rb
+++ lib/librarian/specfile.rb
@@ -1,12 +1,13 @@
 module Librarian
   class Specfile
 
-    attr_reader :path, :dependencies
+    attr_reader :path, :dependencies, :source
 
     def initialize(dsl_class, path)
       @path = path
       spec = dsl_class.run(self)
       @dependencies = spec.dependencies
+      @source = spec.sources.first
     end
 
   end
