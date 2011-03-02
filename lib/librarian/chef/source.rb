423f68d9e42b70491356eddcda68542ef7e601cd
Jay Feldblum <y_feldblum@yahoo.com>
2011-03-02 15:26:42 -0500

    Move Librarian::Chef::Source::Local into its own file.

--- lib/librarian/chef/source.rb
+++ lib/librarian/chef/source.rb
@@ -1,80 +1,11 @@
-require 'fileutils'
-require 'pathname'
-
 require 'librarian/source'
-require 'librarian/manifest'
-require 'librarian/chef/manifest'
+require 'librarian/chef/source/local'
 require 'librarian/chef/source/site'
 require 'librarian/chef/particularity'
 
 module Librarian
   module Chef
     module Source
-
-      module Local
-
-        class Manifest < Manifest
-
-          class << self
-
-            def create(source, dependency, path)
-              manifest?(dependency, path) ? new(source, dependency.name, path) : nil
-            end
-
-            def manifest?(dependency, path)
-              path = Pathname.new(path)
-              manifest_path = manifest_path(path)
-              manifest_path && check_manifest(dependency, manifest_path)
-            end
-
-            def check_manifest(dependency, manifest_path)
-              manifest = read_manifest(manifest_path)
-              manifest["name"] == dependency.name
-            end
-
-          end
-
-          attr_reader :path
-
-          def initialize(source, name, path)
-            super(source, name)
-            @path = Pathname.new(path)
-          end
-
-          def manifest
-            @manifest ||= cache_manifest!
-          end
-
-          def cache_manifest!
-            read_manifest(manifest_path(path))
-          end
-
-          def cache_version!
-            manifest['version']
-          end
-
-          def cache_dependencies!
-            manifest['dependencies']
-          end
-
-          def install!
-            debug { "Installing #{name}-#{version}" }
-            install_path = root_module.install_path.join(name)
-            if install_path.exist?
-              debug { "Deleting #{relative_path_to(install_path)}" }
-              install_path.rmtree
-            end
-            debug { "Copying #{relative_path_to(path)} to #{relative_path_to(install_path)}" }
-            FileUtils.cp_r(path, install_path)
-          end
-
-        end
-
-        def manifest_class
-          Manifest
-        end
-
-      end
 
       class Path < Librarian::Source::Path
         include Particularity
