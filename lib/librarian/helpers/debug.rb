611acc22d7ef9d8f40b8896636e16a2c2d6f1b47
Jay Feldblum <y_feldblum@yahoo.com>
2011-03-02 11:51:31 -0500

    Debugging helpers.

---
+++ lib/librarian/helpers/debug.rb
@@ -0,0 +1,23 @@
+require 'librarian/support/abstract_method'
+
+module Librarian
+  module Helpers
+    module Debug
+
+      include Support::AbstractMethod
+
+      abstract_method :root_module
+
+    private
+
+      def relative_path_to(path)
+        root_module.project_relative_path_to(path)
+      end
+
+      def debug
+        root_module.ui.debug "[Librarian] #{yield}"
+      end
+
+    end
+  end
+end
