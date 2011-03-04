8e94578576b8ee2c7915fb1ce95ad8bc76892757
Jay Feldblum <y_feldblum@yahoo.com>
2011-03-04 09:24:41 -0500

    Move the mock source registry into its own file.

--- lib/librarian/mock/source/mock.rb
+++ lib/librarian/mock/source/mock.rb
@@ -1,71 +1,11 @@
-require 'rubygems'
-
 require 'librarian/manifest'
 require 'librarian/mock/particularity'
+require 'librarian/mock/source/mock/registry'
 
 module Librarian
   module Mock
     module Source
       class Mock
-
-        class Registry
-          module Dsl
-            class Top
-              def initialize(sources)
-                @sources = sources
-              end
-              def source(name, &block)
-                @sources[name] ||= {}
-                Source.new(@sources[name]).instance_eval(&block) if block
-              end
-            end
-            class Source
-              def initialize(source)
-                @source = source
-              end
-              def spec(name, &block)
-                @source[name] ||= []
-                Spec.new(@source[name]).instance_eval(&block) if block
-                @source[name].sort! {|a, b| Gem::Version(a) <=> Gem::Version(b)}.reverse!
-              end
-            end
-            class Spec
-              def initialize(spec)
-                @spec = spec
-              end
-              def version(name, &block)
-                @spec << { :version => name, :dependencies => {} }
-                Version.new(@spec.last[:dependencies]).instance_eval(&block) if block
-              end
-            end
-            class Version
-              def initialize(version)
-                @version = version
-              end
-              def dependency(name, *requirement)
-                @version[name] = requirement
-              end
-            end
-            class << self
-              def run!(sources, &block)
-                Top.new(sources).instance_eval(&block) if block
-              end
-            end
-          end
-          class << self
-            def clear!
-              @sources = nil
-            end
-            def merge!(&block)
-              @sources ||= {}
-              Dsl.run!(@sources, &block) if block
-            end
-            def [](name)
-              @sources ||= {}
-              @sources[name] ||= {}
-            end
-          end
-        end
 
         class Manifest < Manifest
           attr_reader :manifest
---
+++ lib/librarian/mock/source/mock/registry.rb
@@ -0,0 +1,77 @@
+require 'rubygems'
+
+module Librarian
+  module Mock
+    module Source
+      class Mock
+        class Registry
+
+          module Dsl
+
+            class Top
+              def initialize(sources)
+                @sources = sources
+              end
+              def source(name, &block)
+                @sources[name] ||= {}
+                Source.new(@sources[name]).instance_eval(&block) if block
+              end
+            end
+
+            class Source
+              def initialize(source)
+                @source = source
+              end
+              def spec(name, &block)
+                @source[name] ||= []
+                Spec.new(@source[name]).instance_eval(&block) if block
+                @source[name].sort! {|a, b| Gem::Version(a) <=> Gem::Version(b)}.reverse!
+              end
+            end
+
+            class Spec
+              def initialize(spec)
+                @spec = spec
+              end
+              def version(name, &block)
+                @spec << { :version => name, :dependencies => {} }
+                Version.new(@spec.last[:dependencies]).instance_eval(&block) if block
+              end
+            end
+
+            class Version
+              def initialize(version)
+                @version = version
+              end
+              def dependency(name, *requirement)
+                @version[name] = requirement
+              end
+            end
+
+            class << self
+              def run!(sources, &block)
+                Top.new(sources).instance_eval(&block) if block
+              end
+            end
+
+          end
+
+          class << self
+            def clear!
+              @sources = nil
+            end
+            def merge!(&block)
+              @sources ||= {}
+              Dsl.run!(@sources, &block) if block
+            end
+            def [](name)
+              @sources ||= {}
+              @sources[name] ||= {}
+            end
+          end
+
+        end
+      end
+    end
+  end
+end
