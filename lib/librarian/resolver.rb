cdf4a0a67622b6d4aad90e5a9ab7a6d30956005d
Jay Feldblum <y_feldblum@yahoo.com>
2011-03-08 19:23:30 -0500

    A first pass at trying to match version constraints during resolution.

--- lib/librarian/resolver.rb
+++ lib/librarian/resolver.rb
@@ -15,6 +15,99 @@ module Librarian
       end
     end
 
+    class Implementation
+      include Helpers::Debug
+
+      attr_reader :resolver, :source, :dependency_source_map
+
+      def initialize(resolver, spec)
+        @resolver = resolver
+        @source = spec.source
+        @dependency_source_map = Hash[spec.dependencies.map{|d| [d.name, d.source]}]
+        @level = 0
+      end
+
+      def resolve(dependencies, manifests, queue)
+        if dependencies.empty?
+          queue.each do |dependency|
+            debug { "Scheduling #{dependency}" }
+          end
+        end
+        failure = false
+        until failure || queue.empty?
+          dependency = queue.shift
+          dependencies << dependency
+          debug { "Resolving #{dependency}" }
+          scope do
+            if manifests.key?(dependency.name)
+              unless dependency.satisfied_by?(manifests[dependency.name])
+                debug { "Conflicts with #{manifest}" }
+                failure = true
+              else
+                debug { "Accords with all prior constraints" }
+                # nothing left to do
+              end
+            else
+              debug { "No known prior constraints" }
+              resolution = nil
+              related_dependencies = dependencies.select{|d| d.name == dependency.name}
+              debug { "Checking manifests" }
+              scope do
+                dependency.manifests.each do |manifest|
+                  unless resolution
+                    debug { "Checking #{manifest}" }
+                    scope do
+                      #debug { "Dependencies are "}
+                      if related_dependencies.all?{|d| d.satisfied_by?(manifest)}
+                        d = dependencies.dup
+                        m = manifests.merge(dependency.name => manifest)
+                        a = manifest.dependencies.map { |d|
+                          d.source ? d :
+                          !dependency_source_map.key?(d.name) ? Dependency.new(d.name, d.requirement, source) :
+                          Dependency.new(d.name, d.requirement, dependency_source_map[d.name])
+                        }
+                        a.each do |d|
+                          debug { "Scheduling #{d}" }
+                        end
+                        q = queue + a
+                        resolution = resolve(d, m, q)
+                      end
+                      if resolution
+                        debug { "Resolved #{dependency} at #{manifest}" }
+                      end
+                    end
+                  end
+                end
+              end
+              unless resolution
+                failure = true
+              else
+                dependencies, manifests, queue = *resolution
+              end
+            end
+          end
+        end
+        failure ? nil : [dependencies, manifests, queue]
+      end
+
+    private
+
+      def scope
+        @level += 1
+        yield
+      ensure
+        @level -= 1
+      end
+
+      def debug
+        super { '  ' * @level + yield }
+      end
+
+      def root_module
+        resolver.root_module
+      end
+    end
+
     include Helpers::Debug
 
     attr_reader :root_module
@@ -23,27 +116,17 @@ module Librarian
       @root_module = root_module
     end
 
-    def resolve(spec)
-      source = spec.source
-      dependencies = spec.dependencies
-      manifests = {}
-      queue = dependencies.dup
-      until queue.empty?
-        dependency = queue.shift
-        unless manifests.key?(dependency.name)
-          debug { "Resolving #{dependency.name}" }
-          manifest = dependency.manifests.first
-          subdeps = manifest.
-            dependencies.
-            reject{|d| manifests.key?(d.name)}.
-            map{|d| Dependency.new(d.name, d.requirement.as_list, source)}
-          queue.concat(subdeps)
-          manifests[dependency.name] = manifest
-        end
+    def resolve(spec, manifests = {})
+      implementation = Implementation.new(self, spec)
+      resolution = implementation.resolve([], {}, spec.dependencies.dup)
+      unless resolution
+        nil
+      else
+        manifests = resolution[1]
+        manifest_pairs = GraphHash[manifests.map{|k, m| [k, m.dependencies.map{|d| d.name}]}]
+        manifest_names = manifest_pairs.tsort
+        manifest_names.map{|n| manifests[n]}
       end
-      manifest_pairs = GraphHash[manifests.map{|k, m| [k, m.dependencies.map{|d| d.name}]}]
-      manifest_names = manifest_pairs.tsort
-      manifest_names.map{|n| manifests[n]}
     end
 
     def resolved?(dependencies, manifests)
