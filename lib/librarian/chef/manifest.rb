04c03c72be7d60f3429870043504c67f16d779f7
Jay Feldblum <y_feldblum@yahoo.com>
2011-05-09 11:51:21 -0400

    Fix for chef-0.10.

--- lib/librarian/chef/manifest.rb
+++ lib/librarian/chef/manifest.rb
@@ -25,10 +25,11 @@ module Librarian
 
         def compile_manifest(name, path)
           # Inefficient, if there are many cookbooks with uncompiled metadata.
+          require 'chef/json_compat'
           require 'chef/cookbook/metadata'
           md = ::Chef::Cookbook::Metadata.new
           md.name(name)
-          md.from_file(path.join('metadata.rb'))
+          md.from_file(path.join('metadata.rb').to_s)
           JSON.parse(::Chef::JSONCompat.to_json_pretty(md))
         end
 
