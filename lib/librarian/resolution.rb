b07413f26f58da969ddef10ef1ef6a17931fbc32
Jay Feldblum <y_feldblum@yahoo.com>
2011-05-25 11:08:05 -0400

    Split apart the piecies of the Resolution correctness check into their own methods with good names.

--- lib/librarian/resolution.rb
+++ lib/librarian/resolution.rb
@@ -1,26 +1,37 @@
 module Librarian
   class Resolution
-    attr_reader :dependencies, :manifests
+    attr_reader :dependencies, :manifests, :manifests_index
 
     def initialize(dependencies, manifests)
       @dependencies, @manifests = dependencies, manifests
+      @manifests_index = build_manifests_index(manifests)
     end
 
     def correct?
-      manifests && begin
-        manifests_hash = Hash[manifests.map{|m| [m.name, m]}]
-        deps_match = dependencies.all? do |dependency|
-          manifest = manifests_hash[dependency.name]
-          dependency.satisfied_by?(manifest)
-        end
-        mans_match = manifests.all? do |manifest|
-          manifest.dependencies.all? do |dependency|
-            manifest = manifests_hash[dependency.name]
-            dependency.satisfied_by?(manifest)
-          end
-        end
-        deps_match && mans_match
+      manifests && manifests_consistent_with_dependencies? && manifests_internally_consistent?
+    end
+
+  private
+
+    def build_manifests_index(manifests)
+      Hash[manifests.map{|m| [m.name, m]}] if manifests
+    end
+
+    def manifests_consistent_with_dependencies?
+      dependencies.all? do |dependency|
+        manifest = manifests_index[dependency.name]
+        dependency.satisfied_by?(manifest)
       end
     end
+
+    def manifests_internally_consistent?
+      manifests.all? do |manifest|
+        manifest.dependencies.all? do |dependency|
+          manifest = manifests_index[dependency.name]
+          dependency.satisfied_by?(manifest)
+        end
+      end
+    end
+
   end
 end
