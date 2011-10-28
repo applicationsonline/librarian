0808b5f69777afc6bc9e859cd1f5ec92f011068f
Jay Feldblum <y_feldblum@yahoo.com>
2011-10-28 07:38:13 -0400

    And remove the rest of the Librarian delegations, which turn out to be unused.

--- lib/librarian.rb
+++ lib/librarian.rb
@@ -4,20 +4,6 @@ require 'librarian/environment'
 module Librarian
   extend self
 
-  class << self
-    def delegate(*names)
-      options = names.pop
-      to = options.delete(:to)
-      names.each do |name|
-        module_eval <<-CODE, __FILE__, __LINE__ + 1
-          def #{name}(*args, &block)
-            #{to}.#{name}(*args, &block)
-          end
-        CODE
-      end
-    end
-  end
-
   def environment_class
     self::Environment
   end
@@ -25,20 +11,5 @@ module Librarian
   def environment
     @environment ||= environment_class.new
   end
-
-  methods = %w(
-    specfile_name
-    install_path
-    project_path
-    specfile_path
-    specfile
-    lockfile_name
-    lockfile_path
-    lockfile
-    ephemeral_lockfile
-    cache_path
-  )
-
-  delegate *methods, :to => :environment
 
 end
