9a7bc65c90e194cfb6838dddd60e1b784c0c31b4
Jay Feldblum <y_feldblum@yahoo.com>
2012-04-16 09:24:10 -0400

    Enforce the resolver's consistency.
    The resolver must provide a consistent resolution. Otherwise, fail fast and show as much debugging info as possible. Helpful when debugging resolution problems.

--- lib/librarian/resolver.rb
+++ lib/librarian/resolver.rb
@@ -4,6 +4,7 @@ require 'librarian/resolution'
 
 module Librarian
   class Resolver
+    include Helpers::Debug
 
     attr_accessor :environment
     private :environment=
@@ -16,8 +17,33 @@ module Librarian
       implementation = Implementation.new(self, spec)
       partial_manifests_index = Hash[partial_manifests.map{|m| [m.name, m]}]
       manifests = implementation.resolve(spec.dependencies, partial_manifests_index)
+      enforce_consistency!(manifests) if manifests
       manifests = sort(manifests) if manifests
       Resolution.new(spec.dependencies, manifests)
+    end
+
+    def enforce_consistency!(manifests)
+      return if ManifestSet.new(manifests).consistent?
+
+      debug { "Resolver Malfunctioned!" }
+      manifests.values.sort_by(&:name).each do |manifest|
+        errors = []
+        manifest.dependencies.sort_by(&:name).each do |d|
+          if !manifests[d]
+            errors << ["Depends on: #{d}", "Missing!"]
+          elsif !manifests[d].satisfies?(d)
+            errors << ["Depends on: #{d}", "Found: #{manifests[d]}"]
+          end
+        end
+        unless errors.empty?
+          debug { "  #{manifest}" }
+          errors.each do |a, b|
+            debug { "    #{a}" }
+            debug { "      #{b}" }
+          end
+        end
+      end
+      raise Error, "Resolver Malfunctioned!"
     end
 
     def sort(manifests)
