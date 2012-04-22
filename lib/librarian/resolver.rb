419cd18e110d89e54e38cdc5a27d5a87a7e1d970
Jay Feldblum <y_feldblum@yahoo.com>
2012-04-22 15:43:46 -0400

    Complete the consistency check.

--- lib/librarian/resolver.rb
+++ lib/librarian/resolver.rb
@@ -17,22 +17,41 @@ module Librarian
       implementation = Implementation.new(self, spec)
       partial_manifests_index = Hash[partial_manifests.map{|m| [m.name, m]}]
       manifests = implementation.resolve(spec.dependencies, partial_manifests_index)
-      enforce_consistency!(manifests) if manifests
+      enforce_consistency!(spec.dependencies, manifests) if manifests
       manifests = sort(manifests) if manifests
       Resolution.new(spec.dependencies, manifests)
     end
 
-    def enforce_consistency!(manifests)
-      return if ManifestSet.new(manifests).consistent?
+    def enforce_consistency!(dependencies, manifests)
+      return if dependencies.all?{|d|
+        m = manifests[d.name]
+        m && d.satisfied_by?(m)
+      } && ManifestSet.new(manifests).consistent?
 
       debug { "Resolver Malfunctioned!" }
+      errors = []
+      dependencies.sort_by(&:name).each do |d|
+        m = manifests[d.name]
+        if !m
+          errors << ["Depends on #{d}", "Missing!"]
+        elsif !d.satisfied_by?(m)
+          errors << ["Depends on #{d}", "Found: #{m}"]
+        end
+      end
+      unless errors.empty?
+        errors.each do |a, b|
+          debug { "  #{a}" }
+          debug { "    #{b}" }
+        end
+      end
       manifests.values.sort_by(&:name).each do |manifest|
         errors = []
         manifest.dependencies.sort_by(&:name).each do |d|
-          if !manifests[d]
+          m = manifests[d.name]
+          if !m
             errors << ["Depends on: #{d}", "Missing!"]
-          elsif !manifests[d].satisfies?(d)
-            errors << ["Depends on: #{d}", "Found: #{manifests[d]}"]
+          elsif !d.satisfied_by?(m)
+            errors << ["Depends on: #{d}", "Found: #{m}"]
           end
         end
         unless errors.empty?
