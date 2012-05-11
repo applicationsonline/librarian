02602ebd24bc08c1212626fee30accc68eb0351e
Jay Feldblum <y_feldblum@yahoo.com>
2012-05-10 21:16:03 -0400

    Longer timeouts on platforms with slow boots.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,9 @@
 require 'aruba/cucumber'
 
 Before do
-  @aruba_timeout_seconds = 2
+  slow_boot = false
+  slow_boot ||= RUBY_PLATFORM == "java"
+  slow_boot ||= defined?(::Rubinius)
+
+  @aruba_timeout_seconds = slow_boot ? 5 : 2
 end
