3da0de2da3793bc9109754aedf4783db58023ae4
Jay Feldblum <y_feldblum@yahoo.com>
2012-11-11 20:18:16 -0500

    The beginnings of a source linter.
    Useful so far as documentation of required methods.

---
+++ lib/librarian/source/linter.rb
@@ -0,0 +1,54 @@
+module Librarian
+  module Source
+    class Linter
+
+      class << self
+        def lint!(klass)
+          new(klass).lint!
+        end
+      end
+
+      attr_accessor :klass
+      private :klass=
+
+      def initialize(klass)
+        self.klass = klass
+      end
+
+      def lint!
+        lint_class_responds_to! *[
+          :lock_name,
+          :from_spec_args,
+          :from_lock_options,
+        ]
+
+        lint_instance_responds_to! *[
+          :to_spec_args,
+          :to_lock_options,
+          :manifests,
+          :fetch_version,
+          :fetch_dependencies,
+          :pinned?,
+          :unpin!,
+          :install!,
+        ]
+      end
+
+    private
+
+      def lint_class_responds_to!(*names)
+        missing = names.reject{|name| klass.respond_to?(name)}
+        return if missing.empty?
+
+        raise "class must respond to #{missing.join(', ')}"
+      end
+
+      def lint_instance_responds_to!(*names)
+        missing = names - klass.public_instance_methods
+        return if missing.empty?
+
+        raise "instance must respond to #{missing.join(', ')}"
+      end
+    end
+  end
+end
