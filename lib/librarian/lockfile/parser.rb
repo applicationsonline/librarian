e86e80559f8a184a37ad662ec47bbbd82326d4e1
Jay Feldblum <y_feldblum@yahoo.com>
2012-12-29 00:23:55 -0500

    Simplify some of the lockfile parser.

--- lib/librarian/lockfile/parser.rb
+++ lib/librarian/lockfile/parser.rb
@@ -21,10 +21,7 @@ module Librarian
       end
 
       def parse(string)
-        string = string.dup
-        source_type_names_map = Hash[dsl_class.source_types.map{|t| [t[1].lock_name, t[1]]}]
-        source_type_names = dsl_class.source_types.map{|t| t[1].lock_name}
-        lines = string.split(/(\r|\n|\r\n)+/).select{|l| l =~ /\S/}
+        lines = string.lines.map{|l| l.sub(/\s+\z/, '')}.reject(&:empty?)
         sources = []
         while source_type_names.include?(lines.first)
           source = {}
@@ -92,6 +89,18 @@ module Librarian
 
       def dsl_class
         environment.dsl_class
+      end
+
+      def source_type_names_map
+        @source_type_names_map ||= begin
+          Hash[dsl_class.source_types.map{|t| [t[1].lock_name, t[1]]}]
+        end
+      end
+
+      def source_type_names
+        @source_type_names ||= begin
+          dsl_class.source_types.map{|t| t[1].lock_name}
+        end
       end
 
     end
