078e3dd143bcb3c7513eea36f443e34bfb0c8476
Jay Feldblum <y_feldblum@yahoo.com>
2013-03-21 08:41:51 -0400

    Avoid rspec around hooks. Prefer before/after hooks.
    RSpec's around hooks are weird and are not done correctly like ActiveSupport::Callbacks around hooks.

--- spec/support/method_patch_macro.rb
+++ spec/support/method_patch_macro.rb
@@ -10,18 +10,16 @@ module MethodPatchMacro
     def with_module_method(mod, meth, &block)
       tag = SecureRandom.hex(8)
       orig_meth = "_#{tag}_#{meth}".to_sym
-      around do |example|
+      before do
         mod.module_eval do
           alias_method orig_meth, meth
           define_method meth, &block
         end
-        begin
-          example.run
-        ensure
-          mod.module_eval do
-            alias_method meth, orig_meth
-            remove_method orig_meth
-          end
+      end
+      after do
+        mod.module_eval do
+          alias_method meth, orig_meth
+          remove_method orig_meth
         end
       end
     end
